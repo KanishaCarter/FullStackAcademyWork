7ae8c59a2b8a7db3209fc34d260226ded440dbde
KanishaCarter <kanishacarter90@outlook.com>
2025-01-30 21:21:19 -0600

    updating completed workshop (BLOCK7)

---
+++ Unit1/BLOCK7/script.js
@@ -0,0 +1,5 @@
+const code1 = 5 + 5;
+const code2 = 80 / 2;
+const code3 = 40 - 1;
+
+alert("You have received this message because you have been chosen to open an important vault. Here is the secret combination: " + code1 + " - " + code2 + " - " + code3)
